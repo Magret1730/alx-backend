58b29d599cddf6e1ee7e39783a828bf9eac72893
Magret <belloabiodun17@gmail.com>
2024-06-25 14:04:33 +0000

    Task 0 solution

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Task 0 """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    A class imherited from BaseCaching class
+    """
+    def __init__(self):
+        """
+        Initialization of child class
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        assign to the dictionary self.cache_data the item value for the key
+
+        If key or item is None, this method should not do anything.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Must return the value in self.cache_data linked to key.
+
+        If key is None or if the key doesn’t exist in self.cache_data,
+        return None
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
