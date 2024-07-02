2384bd3baa8d98735a6ca3abb1fd44972c9e1673
Magret <oyedeleabiodun79@gmail.com>
2024-07-02 12:28:00 -0230

    Task 0 resolved

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     """ A simple / route """
-    render_template('templates/0-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
