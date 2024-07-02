c52a7b419d665a5e2dafb79e84d7c3f48cf13d5b
Magret <belloabiodun17@gmail.com>
2024-07-02 17:50:50 +0000

    Task 4 solution

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -38,7 +38,7 @@ def get_locale():
 @app.route('/')
 def home():
     """ A simple / route """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
